8391b2296feb6a765223dcfee860eed19d0e853b
Tom Scott <tscott@weblinc.com>
2017-03-29 01:15:22 -0400

    More work on the wonderbars new site

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('music');
+  this.route('video');
+  this.route('live');
 });
 
 export default Router;
