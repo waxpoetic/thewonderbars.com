b875729b9c6f30b8b9040213e25711d36e611bc9
Tom Scott <tscott@weblinc.com>
2017-04-14 22:37:55 -0400

    Slideshow image component

---
+++ app/components/slideshow-image.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tag: 'li',
+  classNames: ['slideshow__slide']
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  setupController(controller, model) {
-    super(controller, model);
-    this.store.findAll('image').then(function(images) {
-      controller.set('images', images);
-    });
+  model() {
+    return this.store.findAll('image');
   }
 });
