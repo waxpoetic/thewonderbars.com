621d1b369ccaf0b45562ce835e328c39e9252988
Tom Scott <tscott@weblinc.com>
2017-04-17 19:30:19 -0400

    Get rid of bower, use Yarn

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import WaxPoeticAdapter from 'ember-cli-waxpoetic/adapters/application'
 
 export default WaxPoeticAdapter.extend({
-  namespace: 'artists/172aad61-87db-4a34-9af5-f6920d9acc93'
+  headers: {
+    'X-Artist': '172aad61-87db-4a34-9af5-f6920d9acc93'
+  }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   setupController(controller, model) {
-    super(controller, model);
+    super.setupController(controller, model);
     this.store.findRecord('artist', 1).then(function(artist) {
       controller.set('artist', artist);
     });
