cee819b9ead6010923c26aceb5d6a63f2c36c413
Tom Scott <tscott@weblinc.com>
2017-05-07 14:02:31 -0400

    Generate components

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // Fall back to using Artist record as the model for most pages
   model() {
     return this.store.findRecord('artist', "5a008b20-a2e9-415c-ae52-be787bcefaf3");
   }
