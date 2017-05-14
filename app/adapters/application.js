166475f3bb00857db3f26a0c58a6909a044d9142
Tom Scott <tscott@weblinc.com>
2017-05-14 10:14:23 -0400

    Fix dependencies

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,7 @@
 import WaxPoeticAdapter from 'ember-cli-waxpoetic/adapters/application'
 
 export default WaxPoeticAdapter.extend({
-  artist: '172aad61-87db-4a34-9af5-f6920d9acc93'
-  // headers: {
-  //   'Artist': '172aad61-87db-4a34-9af5-f6920d9acc93'
-  // }
+  headers: {
+    'Artist': '172aad61-87db-4a34-9af5-f6920d9acc93'
+  }
 });
