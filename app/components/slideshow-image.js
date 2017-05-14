4d978f4d7cca5fd3af135102e58a0b3bfc44920d
Tom Scott <tscott@weblinc.com>
2017-05-14 16:23:25 -0400

    Image captions

--- app/components/slideshow-image.js
+++ app/components/slideshow-image.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tag: 'li',
   classNames: ['slideshow__slide'],
+  caption: '',
   attributeBindings: ['style'],
   style: Ember.computed('src', function() {
     let imageURL = this.get('src');
